9a2a9e8b0c39e95b38c039058f7bce6ccfbad232
EduardoTKX <eduardopeixotocrispim@gmail.com>
2025-12-01 08:23:29 -0300

    Add files via upload

---
+++ flappy bird/main.py
@@ -0,0 +1,34 @@
+import pygame
+from scripts.jogador import Jogador
+from scripts.cenas import Partida
+from scripts.cenas import Menu
+
+pygame.init()
+
+tamanhoTela = [600, 400]
+tela = pygame.display.set_mode(tamanhoTela)
+pygame.display.set_caption("FlappyBird Clone")
+relogio = pygame.time.Clock()
+corFundo = (86, 148, 214)
+jog = Jogador(tela,100,100)
+
+listaCenas = {
+    'partida': Partida(tela),
+    'menu': Menu(tela)
+}
+
+cenaAtual = 'menu'
+jog.atualizar()
+jog.desenhar()
+
+
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+    tela.fill(corFundo)
+
+    cenaAtual = listaCenas[cenaAtual].atualizar()
+
+    relogio.tick(60)
+    pygame.display.flip()
